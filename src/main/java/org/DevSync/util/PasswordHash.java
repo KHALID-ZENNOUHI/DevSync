634057e6b3aa4fdc9d17f878b2b3d397d94c5a41
khalid.zennouhi <144844220+KHALID-ZENNOUHI@users.noreply.github.com>
2024-10-18 16:10:03 +0200

    make methods static

--- src/main/java/org/DevSync/util/PasswordHash.java
+++ src/main/java/org/DevSync/util/PasswordHash.java
@@ -1,16 +1,16 @@
-package org.DevSync.Util;
+package org.DevSync.util;
 
 import org.mindrot.jbcrypt.BCrypt;
 
 public class PasswordHash {
 
     // Hash the plain password using BCrypt
-    public String hashPassword(String plainPassword) {
+    public static String hashPassword(String plainPassword) {
         return BCrypt.hashpw(plainPassword, BCrypt.gensalt(12));
     }
 
     // Check if the plain password matches the hashed password
-    public boolean checkPassword(String plainPassword, String hashedPassword) {
+    public static boolean checkPassword(String plainPassword, String hashedPassword) {
         return BCrypt.checkpw(plainPassword, hashedPassword);
     }
 }
